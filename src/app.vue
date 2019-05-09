32db8b326003032b40488f0ea18edc12d87f2360
tmg2017 <519208032@qq.com>
2019-05-09 10:21:17 +0800

    [U][backup]

--- src/app.vue
+++ src/app.vue
@@ -5,12 +5,7 @@
     "navigationBarBackgroundColor": "#073a64"
   },
   "pages": [
-    "pages/home.vue",
-    "pages/login-code.vue",
-    "pages/login.vue",
-    "pages/all-leads.vue",
-    "pages/lead-form.vue",
-    "pages/qrcode.vue"
+    "pages/home.vue"
   ]
 }
 </config>
